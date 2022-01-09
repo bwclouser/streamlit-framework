b602a9c29845b0c692d252a04d63f2af947d5bfa
bwclouser <93306018+bwclouser@users.noreply.github.com>
2022-01-08 23:42:22 -0600

    Now with a return!

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    HttpResponse('<pre>'+'PotatoPotato'+'<\pre>')
+    return HttpResponse('<pre>'+'PotatoPotato'+'<\pre>')
