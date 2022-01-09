bcbda75fd28c5fa6792c9a6ea36437ce5f497d3c
bwclouser <93306018+bwclouser@users.noreply.github.com>
2022-01-08 23:50:17 -0600

    another test

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Greeting
+
 def index(request):
     return HttpResponse('<pre>'+'PotatoPotato'+'<\pre>')
