fac8b73cff3c0625a05f4914afe83e85bdaad96b
bwclouser <93306018+bwclouser@users.noreply.github.com>
2022-01-09 22:38:50 -0600

    Working Example

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from bokeh.plotting import figure,output_file,show
+from datetime import datetime
 
 st.title("Test Program")
 
@@ -15,11 +16,19 @@ url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+
 r = requests.get(url)
 data = r.json()
 
-df = pd.DataFrame.from_dict(data, orient = 'index')
+#df = pd.DataFrame.from_dict(data, orient = 'index')
 
-#p = figure()
-#show(p)
+dict=data['Time Series (5min)']
 
-fig = plt.figure()
-plt.title=user_input + ' Price'
-st.pyplot(fig)
+times=dict.keys()
+dtime=[datetime.strptime(time, '%Y-%m-%d %H:%M:%S') for time in times]
+
+closes=[float(val['4. close']) for val in dict.values()]
+
+print(dtime[0],closes[0])
+
+
+p = figure(title=user_input + ' Stock Price',x_axis_label='Date',y_axis_label='Price (USD)')
+p.line(dtime, closes, line_width=2)
+
+st.bokeh_chart(p, use_container_width=True)
