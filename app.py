8f27fda6d6c0ae69a8a2dfab793ce1b624d4dc9b
bwclouser <93306018+bwclouser@users.noreply.github.com>
2022-01-10 22:28:28 -0600

    added selectbox

--- app.py
+++ app.py
@@ -8,10 +8,27 @@ from datetime import timedelta
 import os
 
 st.title("Milestone Project -- Stock Prices")
+st.text("Display the previous month of stock data")
 
 #st.sidebar.markdown('## Ticker Symbol')
 
+values=['Closing Price', 'Opening Price', 'Volume']
+default_ix=values.index('Closing Price')
+
 user_input = st.sidebar.text_input("Ticker Symbol", 'GOOG')
+option = st.sidebar.selectbox('Data Options',values,index=default_ix)
+
+#print(option,default_ix)
+
+if option=='Closing Price':
+    dchoice = '4. close'
+    yAxLab = 'Price (USD)'
+elif option=='Opening Price':
+    dchoice = '1. open'
+    yAxLab = 'Price (USD)'
+else:
+    dchoice = '5. volume'
+    yAxLab = 'Shares'
 
 apikey = os.environ.get('APIKEY')
 #print(apikey)
@@ -27,7 +44,7 @@ dict=data['Time Series (60min)']
 times=dict.keys()
 dtime=np.array([datetime.strptime(time, '%Y-%m-%d %H:%M:%S') for time in times])
 
-closes=np.array([float(val['4. close']) for val in dict.values()])
+closes=np.array([float(val[dchoice]) for val in dict.values()])
 
 truth=dtime>dtime[0]-timedelta(days=31)
 
@@ -37,7 +54,7 @@ closes=closes[truth]
 print(dtime[0],closes[0])
 
 
-p = figure(title=user_input + ' Stock Price',x_axis_label='Date',y_axis_label='Price (USD)')
+p = figure(title=user_input + ' ' + option,x_axis_label='Date',y_axis_label=yAxLab)
 p.line(dtime, closes, line_width=2)
 p.xaxis.formatter=DatetimeTickFormatter(years=["%d %B %Y"])
 st.bokeh_chart(p, use_container_width=True)
